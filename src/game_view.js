d582d3067ae827d02ac485abe2e2b5bfe92e7425
Eugene Birondo <jbirondo@gmail.com>
2019-12-13 11:35:26 -0800

    Debugging gaveView.start cont.

--- src/game_view.js
+++ src/game_view.js
@@ -15,7 +15,7 @@ class GameView {
     };
 
     start() {
-        while( lives > 0 || this.game.enemies.length === 0) {
+        if( lives > 0 || this.game.enemies.length === 0) {
             console.log("in gameView.start")
             this.game.play()
         }
