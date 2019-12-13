9ea2839ad0377bc13e1740f23322d21573699521
Eugene Birondo <jbirondo@gmail.com>
2019-12-13 11:25:40 -0800

    Debugging gaveView.start cont.

--- dist/main.js
+++ dist/main.js
@@ -398,7 +398,7 @@ class GameView {
     };
 
     start() {
-        if( lives > 0 || this.game.enemies.length === 0) {
+        while( lives > 0 && this.game.enemies.length === 0) {
             console.log("in gameView.start")
             this.game.play()
         }
