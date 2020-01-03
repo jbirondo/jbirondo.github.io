a0ae31ba2173d535bd754f2b5decfe489559c076
Eugene Birondo <jbirondo@gmail.com>
2020-01-03 13:08:05 -0800

    Temp changes to add instructions

--- src/index.js
+++ src/index.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const instructions = document.getElementById("instructions")
     const instructionsContext = instructions.getContext("2d")
     const instructionsEle = new Instructions(instructionsContext)
-    new InstructionsView(instructionsEle, instructionsContext).start()
+    new InstructionsView(instructionsEle).start()
 
     const statsClick = (event) => {
         let pos = getMousePos(canvas, event)
