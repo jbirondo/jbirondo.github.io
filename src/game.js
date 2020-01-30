8d5d790af335fecfd874bcfd0f8d817507b35332
Eugene Birondo <jbirondo@gmail.com>
2020-01-30 14:44:33 -0800

    Debugging game.play

--- src/game.js
+++ src/game.js
@@ -166,24 +166,29 @@ class Game {
     spawn() {
         let enemies = this.createEnemyArr();
         let counter = 0;
-        let spawnWave = setInterval(() => {
-            if (counter < enemies.length) {
-                this.spawnFunc(enemies, counter)
-                counter++;
-                console.log("hello from game.spawn if statement", counter)
-            } else {
-                clearInterval(spawnWave)
-                counter = 0
-                console.log("hello from game.spawn else statement", counter)
-            }
-        }, 1000);
+        let gameLoop = setInterval(() => {
+            let spawnWave = setInterval(() => {
+                if (counter < enemies.length) {
+                    this.spawnFunc(enemies, counter)
+                    counter++;
+                    console.log("hello from game.spawn if statement", counter)
+                } else {
+                    clearInterval(spawnWave)
+                    counter = 0
+                    console.log("hello from game.spawn else statement", counter)
+                }
+            }, 1000);
+            console.log("game.spawn in gameLoop")
+        }, 15000);
         // spawnWave
         while (this.enemies.length === 0 && lives > 0){
-            setInterval(() => {
-                console.log("game.spawn while lopp")
-                spawnWave
-            }, 15000);
+            gameLoop
         }
+        //     setInterval(() => {
+        //         console.log("game.spawn while lopp")
+        //         spawnWave
+        //     }, 15000);
+        // }
         // let spawnOne = setInterval(() => {
         //     this.spawnFunc(enemies, counter)
         // }, 1000);
