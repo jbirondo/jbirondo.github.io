576bf708c63d2d643414c1e00184206a31197228
Eugene Birondo <jbirondo@gmail.com>
2020-02-05 23:22:17 -0800

    Working on enemy stats

--- src/enemy_stats.js
+++ src/enemy_stats.js
@@ -1,14 +1,22 @@
 class EnemyStats {
-    constructor(context) {
+    constructor(context, enemy) {
         this.context = context
+        this.enemy = enemy
+        this.hp = enemy.hp
+        this.speed = enemy.speed    
     }
 
     draw() {
-        this.context.clearRect(0, 0, 300, 100)
+        // this.context.clearRect(0, 0, 300, 100)
+        // this.context.font = "20px Arial";
+        // this.context.fillText(`Score: ${score}`, 10, 20);
+        // this.context.fillText(`Lives: ${lives}`, 10, 50)
+        // this.context.fillText(`Round: ${round}`, 10, 80)
+        this.context.clearRect(0, 0, 300, 300)
         this.context.font = "20px Arial";
-        this.context.fillText(`Score: ${score}`, 10, 20);
-        this.context.fillText(`Lives: ${lives}`, 10, 50)
-        this.context.fillText(`Round: ${round}`, 10, 80)
+        this.context.fillText(`Enemy`, 10, 80)
+        this.context.fillText(`Speed: ${object.speed}`, 10, 110)
+        this.context.fillText(`Health: ${this.hp}`, 10, 140)
     }
 }
 
