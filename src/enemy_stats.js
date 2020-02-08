fb67e46c0f7073c7d21d12df8e161ea5b168bbd0
Eugene Birondo <jbirondo@gmail.com>
2020-02-07 16:41:18 -0800

    Issues with countdown

--- src/enemy_stats.js
+++ src/enemy_stats.js
@@ -11,7 +11,7 @@ class EnemyStats {
             this.context.fillText(`${startTime}`, 10, 110)
             setInterval(() => {
                 startTime--
-            }, 1000);
+            }, 10000);
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
