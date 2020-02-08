5569ce3bb2cc73580b86fd65e6b1467fc9ab8e10
Eugene Birondo <jbirondo@gmail.com>
2020-02-07 17:08:43 -0800

    Issues with countdown

--- dist/main.js
+++ dist/main.js
@@ -189,31 +189,7 @@ module.exports = Enemy
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-class EnemyStats {
-    constructor(context) {
-        this.context = context  
-    }
-
-    draw() {
-        setInterval(() => {
-            if (round === 0){
-                this.context.clearRect(0, 0, 300, 300)
-                this.context.font = "20px Arial";
-                this.context.fillText(`Round starts in:`, 10, 80)
-                this.context.fillText(`${startTime}`, 10, 110)
-                startTime--
-            } 
-        }, 1000);
-        if(round > 0) {
-            this.context.clearRect(0, 0, 300, 300)
-            this.context.font = "20px Arial";
-            this.context.fillText(`Enemy`, 10, 80)
-            this.context.fillText(`Health: ${(12 * (1 + (0.25 * (round - 1))) )}`, 10, 110)
-        }
-    }
-}
-
-module.exports = EnemyStats;
+throw new Error("Module parse failed: Unexpected token (6:8)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n|     }\n| \n>     num = 35;\n|     intvl = setInterval(function () { counter(ctx, num--); }, 1000);\n|     counter(num) {");
 
 /***/ }),
 
--- src/enemy_stats.js
+++ src/enemy_stats.js
@@ -3,17 +3,26 @@ class EnemyStats {
         this.context = context  
     }
 
+    num = 35;
+    intvl = setInterval(function () { counter(ctx, num--); }, 1000);
+    counter(num) {
+        this.context.clearRect(0, 0, 300, 300)
+        this.context.font = "20px Arial";
+        this.context.fillText(`Round starts in:`, 10, 80)
+        this.context.fillText(`${num}`, 10, 110)
+        if (num == 0) {
+            clearInterval(intvl);
+        }
+    } 
+
     draw() {
-        setInterval(() => {
-            if (round === 0){
-                this.context.clearRect(0, 0, 300, 300)
-                this.context.font = "20px Arial";
-                this.context.fillText(`Round starts in:`, 10, 80)
-                this.context.fillText(`${startTime}`, 10, 110)
-                startTime--
-            } 
-        }, 1000);
-        if(round > 0) {
+        if (round === 0){
+            // this.context.clearRect(0, 0, 300, 300)
+            // this.context.font = "20px Arial";
+            // this.context.fillText(`Round starts in:`, 10, 80)
+            // this.context.fillText(`${startTime}`, 10, 110)
+            this.counter(num)
+        } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
             this.context.fillText(`Enemy`, 10, 80)
