2d99f0247c9ac5db173442e7918fdbd35305b5bb
Eugene Birondo <jbirondo@gmail.com>
2020-02-10 22:07:48 -0800

    Reworking countdown

--- src/enemy_stats.js
+++ src/enemy_stats.js
@@ -8,7 +8,10 @@ class EnemyStats {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
             this.context.fillText(`Round starts in:`, 10, 80)
-            this.context.fillText(`${startTime}`, 10, 110)
+            setInterval(() => {
+                this.context.fillText(`${startTime}`, 10, 110)
+                startTime--
+            }, 1000);
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
