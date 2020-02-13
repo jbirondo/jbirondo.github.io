0a76d6bde821049026a0ea35015a37a0dbb4bafd
Eugene Birondo <jbirondo@gmail.com>
2020-02-12 23:23:55 -0800

    EnemyStats.draw

--- dist/main.js
+++ dist/main.js
@@ -196,11 +196,12 @@ class EnemyStats {
 
     draw() {
 
-        if (round === 0){
+        if (round === 0 && startTime >= 0){
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
             this.context.fillText(`Round starts in:`, 10, 80)
             this.context.fillText(`${startTime}`, 10, 110)
+            startTime--
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
