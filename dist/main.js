a1c071c7206629a94a91d4dd41a789e4b086e16b
Eugene Birondo <jbirondo@gmail.com>
2020-02-13 23:21:11 -0800

    Reverting changes

--- dist/main.js
+++ dist/main.js
@@ -194,24 +194,24 @@ class EnemyStats {
         this.context = context  
     }
     
-    countdown() {
-        let num = 35
-        setInterval(() => {
-            console.log(num)
-            num--
-        }, 1000);
-    }
+    // countdown() {
+    //     let num = 35
+    //     setInterval(() => {
+    //         console.log(num)
+    //         num--
+    //     }, 1000);
+    // }
 
     draw() {
         // debugger
 
         if (round === 0){
-            // this.context.clearRect(0, 0, 300, 300)
-            // this.context.font = "20px Arial";
-            // this.context.fillText(`Round starts in:`, 10, 80)
-            // this.context.fillText(`${startTime}`, 10, 110)
+            this.context.clearRect(0, 0, 300, 300)
+            this.context.font = "20px Arial";
+            this.context.fillText(`Round starts in:`, 10, 80)
+            this.context.fillText(`${startTime} seconds`, 10, 110)
             // startTime--
-            this.countdown()
+            // this.countdown()
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
