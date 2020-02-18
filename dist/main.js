ea5c26dd7203afda62ef7fb965217457e48661c3
Eugene Birondo <jbirondo@gmail.com>
2020-02-17 23:20:15 -0800

    Countdown working

--- dist/main.js
+++ dist/main.js
@@ -197,10 +197,17 @@ class EnemyStats {
     draw() {
         if (round === 0){
             let a = Date.now()
+            let countdown = Math.floor((startTime - a) / 1000)
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
             this.context.fillText(`Round starts in:`, 10, 80)
-            this.context.fillText(`${Math.floor((startTime - a) / 1000)} seconds`, 10, 110)
+            if (countdown === 1){
+                this.context.fillText(`${countdown} second`, 10, 110)
+            }
+            if (countdown === 0){
+                this.context.fillText(`NOW`, 10, 110)
+            }
+            this.context.fillText(`${countdown} seconds`, 10, 110)
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
