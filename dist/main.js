f853b68ffcb80585a85c5234c5ba9839ccd7cf64
Eugene Birondo <jbirondo@gmail.com>
2020-02-17 22:45:20 -0800

    Reverting changes

--- dist/main.js
+++ dist/main.js
@@ -193,25 +193,14 @@ class EnemyStats {
     constructor(context) {
         this.context = context  
     }
-    
-    countdown() {
-        let num = 35
-        setInterval(() => {
-            console.log(num)
-            num--
-        }, 1000);
-    }
 
     draw() {
-        debugger
-
         if (round === 0){
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
             this.context.fillText(`Round starts in:`, 10, 80)
             this.context.fillText(`${startTime} seconds`, 10, 110)
             startTime--
-            this.countdown()
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
