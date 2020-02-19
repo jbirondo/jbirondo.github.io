2658a15c66d0a3a4b817512c64226e1f392cc537
Eugene Birondo <jbirondo@gmail.com>
2020-02-18 23:24:30 -0800

    Modifications to countdown timer

--- dist/main.js
+++ dist/main.js
@@ -198,10 +198,22 @@ class EnemyStats {
         if (round === 0){
             let a = Date.now()
             let countdown = Math.floor((startTime - a) / 1000)
-            this.context.clearRect(0, 0, 300, 300)
-            this.context.font = "20px Arial";
-            this.context.fillText(`Round starts in:`, 10, 80)
-            this.context.fillText(`${countdown} seconds`, 10, 110)
+            if(countdown > 1){
+                this.context.clearRect(0, 0, 300, 300)
+                this.context.font = "20px Arial";
+                this.context.fillText(`Round starts in:`, 10, 80)
+                this.context.fillText(`${countdown} seconds`, 10, 110)
+            } else if (countdown === 1) {
+                this.context.clearRect(0, 0, 300, 300)
+                this.context.font = "20px Arial";
+                this.context.fillText(`Round starts in:`, 10, 80)
+                this.context.fillText(`${countdown} second`, 10, 110)
+            } else if (countdown < 1){
+                this.context.clearRect(0, 0, 300, 300)
+                this.context.font = "20px Arial";
+                this.context.fillText(`Round starts NOW!`, 10, 80)
+                // this.context.fillText(`${countdown} second`, 10, 110)
+            }
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
