22e107de3febe47528d662cbe76eeee005c122e9
Eugene Birondo <jbirondo@gmail.com>
2020-02-24 11:11:48 -0800

    Modifying movingobject.draw

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,7 +1,7 @@
 const Util = require("./util")
 
 class MovingObject {
-    constructor(x, y, dx, dy, radius, color, game) {
+    constructor(x, y, dx, dy, radius, hp, color, game) {
         this.x = x;
         this.y = y;
         this.dx = dx;
@@ -9,6 +9,7 @@ class MovingObject {
         this.radius = radius;
         this.color = color;
         this.game = game;
+        this.hp = hp
         this.NORMAL_FRAME_TIME_DELTA = 1000 / 60
     }
 
@@ -16,7 +17,7 @@ class MovingObject {
         context.fillStyle = this.color;
         context.beginPath();
         context.arc(
-            this.x, this.y, this.radius, 0, 2 * Math.PI, true
+            this.x, this.y, this.radius, 0, (2 * Math.PI) / (12 * (1 + (0.25 * (round - 1))) / this.hp), true
         );
         context.fill();
 
