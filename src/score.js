0c6b479d85ecc716e033487128cf62977618272a
Eugene Birondo <jbirondo@gmail.com>
2020-03-04 12:53:05 -0800

    Countdown moved to score, adjusting placement

--- src/score.js
+++ src/score.js
@@ -15,9 +15,9 @@ class Score {
             this.context.fillText(`Prepare!`, 25, 50);
             this.context.beginPath();
             this.context.arc(
-                60,
-                60,
                 50,
+                50,
+                40,
                 0,
                 (2 * Math.PI) * (1 + (-1 * (countdown / 35))),
                 true
