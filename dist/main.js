ea3383bded3e332145e13af97799ef1d01d990c5
Eugene Birondo <jbirondo@gmail.com>
2020-03-04 15:43:09 -0800

    Countdown moved to score, adjusting placement

--- dist/main.js
+++ dist/main.js
@@ -730,7 +730,7 @@ class Score {
             this.context.fillText(`Score: ${score}`, 120, 20);
             this.context.fillText(`Lives: ${lives}`, 120, 50);
             this.context.fillText(`Wave`, 25, 50);
-            this.context.fillText(`${round}`, 50, 50)
+            this.context.fillText(`${round}`, 50, 70)
             this.context.beginPath();
             this.context.arc(
                 50,
