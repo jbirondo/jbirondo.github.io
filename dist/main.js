d626552eab31ec5f78766f44134a69c89890165b
Eugene Birondo <jbirondo@gmail.com>
2020-03-04 22:42:40 -0800

    Reverting gradient changes

--- dist/main.js
+++ dist/main.js
@@ -873,6 +873,7 @@ class Tower {
     draw(context) {
         context.fillStyle = "red"
         context.strokeRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
+        context.lineWidth = 3
         context.fillRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
     }
 
