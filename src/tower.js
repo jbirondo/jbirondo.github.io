cb7b403257b31254bff1d246f2f307170b8b2f87
Eugene Birondo <jbirondo@gmail.com>
2020-03-05 20:59:37 -0800

    Starting tower upgrades

--- src/tower.js
+++ src/tower.js
@@ -14,6 +14,7 @@ class Tower {
         // context.fillStyle = "red"
         // context.strokeRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
         // context.fillRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
+        debugger
         context.font = "14px Arial"
         context.fillText("♟", this.row * tileSize, (this.col * tileSize) + tileSize - 4)
     }
