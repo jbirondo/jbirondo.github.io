0d2ac6000382e81d008d40f04c85c1f17c0a134b
Eugene Birondo <jbirondo@gmail.com>
2020-03-05 22:40:19 -0800

    Starting tower upgrades

--- src/stats.js
+++ src/stats.js
@@ -16,7 +16,6 @@ class Stats {
             this.context.fillText(`Range: ${object.range}`, 10, 110)
             this.context.fillText(`Damage: ${object.dmg}`, 10, 140)
         } else {
-            debugger
             this.context.clearRect(0, 0, 300, 300)
             // this.context.font = "20px Arial";
             // this.context.fillText(`Enemy`, 10, 80)
