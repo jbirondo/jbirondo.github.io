1657862a0e49176255b39f15c84c5c697b887bac
Eugene Birondo <jbirondo@gmail.com>
2020-03-06 00:34:22 -0800

    Making upgrade button

--- dist/main.js
+++ dist/main.js
@@ -847,7 +847,7 @@ class Stats {
         if (object instanceof Tower) {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
-            this.context.strokeStyle = "black"
+            this.context.fillStyle = "black"
             this.context.fillText(`Coords : x:${object.row} y:${object.col}`, 10, 20)
             this.context.fillText(`${object.stage} Tower`, 10, 80)
             this.context.fillText(`Range: ${object.range}`, 10, 110)
