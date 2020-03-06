25b5660820d0bbc11ff470785b2dfc35b90ba449
Eugene Birondo <jbirondo@gmail.com>
2020-03-06 00:31:41 -0800

    Making upgrade button

--- src/stats.js
+++ src/stats.js
@@ -16,12 +16,14 @@ class Stats {
             this.context.fillText(`Range: ${object.range}`, 10, 110)
             this.context.fillText(`Damage: ${object.dmg}`, 10, 140)
             if (object.stage === "Pawn") {
-                this.context.strokeStyle = "blue"
+                this.context.fillStyle = "blue"
                 this.context.fillText(`Upgrade`, 14, 40)
+                this.context.strokeStyle = "blue"
                 this.context.strokeRect(10, 23, 85, 22);
-                this.context.strokeStyle = "red"
+                this.context.fillStyle = "red"
                 this.context.fillText(`Sell`, 104, 40)
-                this.context.strokeRect(100, 23, 38, 22);
+                this.context.strokeStyle = "red"
+                this.context.strokeRect(100, 23, 40, 22);
             }
             // this.context.fillRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
         } 
