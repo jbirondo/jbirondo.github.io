93b331838f9f6f866cc09d51a9edf708b7926d5a
Eugene Birondo <jbirondo@gmail.com>
2020-03-05 23:59:48 -0800

    Making upgrade button

--- dist/main.js
+++ dist/main.js
@@ -852,9 +852,10 @@ class Stats {
             this.context.fillText(`Range: ${object.range}`, 10, 110)
             this.context.fillText(`Damage: ${object.dmg}`, 10, 140)
             if (object.stage === "Pawn") {
-                this.context.strokeStyle = "blue"
-                this.context.fillText(`Upgrade`, 14, 40)
-                this.context.strokeRect(10, 24, 85, 20);
+                this.context.strokeStyle = "black"
+                this.context.strokeText(`Upgrade`, 14, 40)
+                this.context.fillStyle = "blue"
+                this.context.fillRect(10, 23, 85, 22);
             }
             // this.context.fillRect(this.row * tileSize, this.col * tileSize, tileSize, tileSize);
         } 
