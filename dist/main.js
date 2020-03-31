c4cc94313ae3f56deb0641799b24476927feb768
Eugene Birondo <jbirondo@gmail.com>
2020-03-30 22:22:58 -0700

    Adding social links

--- dist/main.js
+++ dist/main.js
@@ -624,10 +624,11 @@ class Instructions {
     }
 
     draw() {
-        this.context.clearRect(0, 0, 900, 200)
+        this.context.clearRect(0, 0, 900, 80)
         this.context.font = "20px Arial";
         this.context.fillText(`Welcome to JavaScript Tower Defense`, 20, 30);
         this.context.fillText(`Left click on a green tile to create a tower to defend the path`, 20, 60)
+
     }
 }
 
