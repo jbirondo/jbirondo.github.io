25e9639974450b5d8551605253b438911c61888e
Eugene Birondo <jbirondo@gmail.com>
2020-03-31 21:13:13 -0700

    Adding social links

--- src/instructions.js
+++ src/instructions.js
@@ -8,7 +8,7 @@ class Instructions {
         this.context.font = "20px Arial"
         this.context.fillText(`Welcome to JavaScript Tower Defense`, 20, 30);
         this.context.fillText(`Left click on a green tile to create a tower to defend the path`, 20, 60)
-        this.context.font = "14px FontAwesome";
+        this.context.font = "20px FontAwesome";
         this.context.fillText('\uf09b', 20, 90)
     }
 }
