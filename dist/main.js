fdd71f9ac19dbfde376b49bcb1c00a4333acefd2
Eugene Birondo <jbirondo@gmail.com>
2020-04-03 10:36:55 -0700

    Adding social links

--- dist/main.js
+++ dist/main.js
@@ -600,6 +600,7 @@ class Instructions {
     draw() {
         this.context.clearRect(0, 0, 900, 120)
         this.context.font = "20px Arial"
+        this.context.fillStyle = "black"
         this.context.fillText(`Welcome to JavaScript Tower Defense`, 20, 30);
         this.context.fillText(`Left click on a green tile to create a tower to defend the path`, 20, 60)
         this.context.font = "30px FontAwesome";
