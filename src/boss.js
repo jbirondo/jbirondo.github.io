b1236c4133fccebedff7b7f996ef378cc2b8f42b
Eugene Birondo <jbirondo@gmail.com>
2020-11-19 11:46:01 -0800

    Boss class added to game.play() but need to configure stats view and arc

--- src/boss.js
+++ src/boss.js
@@ -4,6 +4,8 @@ class Boss extends Enemy {
     constructor(x, y, dx, dy, radius, color, game){
         super(x, y, dx, dy, radius, color, game)
         this.speed = 3;
+        this.dx = 2
+        this.dy = 2
         this.hp = 10 * round
         this.game = game
         this.color = "blue"
