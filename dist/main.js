2f9e36bb51653c9b8ccc4fe84b21c5edf30021e4
Eugene Birondo <jbirondo@gmail.com>
2020-11-18 22:59:58 -0800

    Creating Boss class

--- dist/main.js
+++ dist/main.js
@@ -99,7 +99,7 @@ class Boss extends Enemy {
     constructor(x, y, dx, dy, radius, color, game){
         super(x, y, dx, dy, radius, color, game)
         this.speed = 3;
-        this.hp = round * round
+        this.hp = 10 * round * round
         this.game = game
         this.bounty = round * round
     }
