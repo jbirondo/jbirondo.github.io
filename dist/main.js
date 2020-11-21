5a825b42b72599c122637e74268d0a6c80e72474
Eugene Birondo <jbirondo@gmail.com>
2020-11-20 23:54:40 -0800

    Testing

--- dist/main.js
+++ dist/main.js
@@ -817,14 +817,15 @@ class MovingObject {
         if (Math.floor(totalHp) !== Math.floor(this.hp)){
             context.fillStyle = this.color;
             context.beginPath();
-            context.arc(
-                this.x, 
-                this.y, 
-                this.radius, 
-                0, 
-                (2 * Math.PI) * (1 + (-1 * (this.hp / totalHp))), 
-                true
-            );
+            // context.arc(
+            //     this.x, 
+            //     this.y, 
+            //     this.radius, 
+            //     0, 
+            //     (2 * Math.PI) * (1 + (-1 * (this.hp / totalHp))), 
+            //     true
+            // );
+            context.globalAlpha = Math.floor(this.hp / totalHp)
             context.fill();
         } else if (Math.floor(totalHp) === Math.floor(this.hp)) {
             context.fillStyle = this.color;
