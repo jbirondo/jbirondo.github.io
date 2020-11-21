9863abd24118a77ebbc4d20729b74a3e81b90537
Eugene Birondo <jbirondo@gmail.com>
2020-11-21 00:43:59 -0800

    Testing

--- dist/main.js
+++ dist/main.js
@@ -825,6 +825,7 @@ class MovingObject {
                 (2 * Math.PI), //* (1 + (-1 * (this.hp / totalHp))), 
                 true
             );
+            context.globalAlpha = this.hp / totalHp
             context.fill();
         } else if (Math.floor(totalHp) === Math.floor(this.hp)) {
             context.fillStyle = this.color;
