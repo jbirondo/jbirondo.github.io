d75ef1df39db9da896a8a724c9498bce839a9390
Eugene Birondo <jbirondo@gmail.com>
2020-11-23 22:45:56 -0800

    Boss enemystats

--- dist/main.js
+++ dist/main.js
@@ -274,6 +274,11 @@ class EnemyStats {
     draw() {
         if (round === 0){
         null
+        } else if (round % 5 === 0){
+            this.context.clearRect(0, 0, 300, 300)
+            this.context.font = "20px Arial";
+            this.context.fillText(`Boss`, 10, 80)
+            this.context.fillText(`Health: ${(10 * round )}`, 10, 110)
         } else {
             this.context.clearRect(0, 0, 300, 300)
             this.context.font = "20px Arial";
