c2705a321a0e9716dfec03194a91a48e0c4b3a81
Eugene Birondo <jbirondo@gmail.com>
2020-11-25 20:32:33 -0800

    Changing sounds

--- dist/main.js
+++ dist/main.js
@@ -350,6 +350,7 @@ class Game {
             this.projectiles.push(object);
         } else if (object instanceof Tower) {
             this.towers.push(object);
+            this.playSound('click.wav')
             money -= object.cost 
         } else {
             throw new Error("unknown type of object");
